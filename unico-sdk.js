1d723ee85f737b2c7f5c0eb0fbbcd9323f04f6a8
Icaro Almeida <icaroalmeida@hotmail.com>
2023-12-12 16:41:55 -0300

    alteração unico-sdk.js

--- unico-sdk.js
+++ unico-sdk.js
@@ -1,42 +1,57 @@
-import { UnicoCheckBuilder, SelfieCameraTypes, UnicoThemeBuilder, DocumentCameraTypes } from './UnicoCheckBuilder.min.js'
-/*import axios from 'axios';*/
+import {
+    UnicoCheckBuilder,
+    SelfieCameraTypes,
+    UnicoThemeBuilder,
+    DocumentCameraTypes,
+} from "./UnicoCheckBuilder.min.js";
+
 var callback = {
     on: {
         success: function (obj) {
-
             // Realiza a requisição com os dados
             // da imagem para o endpoint (api2)
             createProcess(obj);
 
-            axios.post('https://sboxgestor.bubbleapps.io/version-test/api/1.1/wf/recieve_selfie/initialize', obj)
-                .then(response => {
-                    res.end();
+            fetch(
+                "https://sboxgestor.bubbleapps.io/version-test/api/1.1/wf/recieve_selfie/initialize",
+                {
+                    method: 'POST',
+                    body: JSON.stringify(obj),
+                    headers: { 'Content-Type': 'application/json' }
+                }
+            )
+                .then((response) => {
+                    if (!response.ok) {
+                        throw new Error('Network response was not ok');
+                    }
                 })
-                .catch(error => {
-                    res.end();
+                .catch((error) => {
+                    console.error('There has been a problem with your fetch operation:', error);
                 });
-
         },
         error: function (error) {
-            //confira na aba "Configura��es" sobre os tipos de erros
-        }
-    }
+            console.log(error);
+            //confira na aba "Configura  es" sobre os tipos de erros
+        },
+    },
 };
 
 const unicoCameraBuilder = new UnicoCheckBuilder();
-unicoCameraBuilder.setResourceDirectory('/resources');
-unicoCameraBuilder.setModelsPath('/models');
+unicoCameraBuilder.setResourceDirectory("/resources");
+unicicoCameraBuilder.setModelsPath("/models");
 
-const unicoTheme = new UnicoThemeBuilder()
-    .build();
+const unicoTheme = new UnicoThemeBuilder().build();
 
 unicoCameraBuilder.setTheme(unicoTheme);
 
 const unicoCamera = unicoCameraBuilder.build();
-const cameraPromised = unicoCamera.prepareSelfieCamera('/services.json', SelfieCameraTypes.SMART);
-cameraPromised.then(cameraOpener => cameraOpener.open(callback))
+const cameraPromised = unicoCamera.prepareSelfieCamera(
+    "/services.json",
+    SelfieCameraTypes.SMART
+);
+cameraPromised.then((cameraOpener) => cameraOpener.open(callback));
 
 function createProcess(unico) {
-    console.log(unico);
-    /*   window.parent.postMessage(objetoGerado, 'https://zipboxcard.com.br/version-test/captura_selfie');*/
+    console.log(unico)
+
 }
